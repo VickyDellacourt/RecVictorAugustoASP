b1edd24ae58275ffab303830b5cc9ffa8bfee02a
GuilhermeMartinez <guigui.martinez20@hotmail.com>
2018-05-05 21:00:11 -0300

    ASP

---
+++ RecVictorAugustoASP/Models/DataAccessObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RecVictorAugustoASP.Models
+{/// <summary>
+/// Chamada do Banco Modelo para Tratar (DAO)
+/// </summary>
+    public class DataAccessObject:DbContext
+    {
+        public DataAccessObject() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<RecVictorAugustoASP.Models.Category> Categories { get; set; }
+
+        public System.Data.Entity.DbSet<RecVictorAugustoASP.Models.News> News { get; set; }
+
+    }
+}
